5dd45c74ec06b3018181e51ed9191917b4605d49
ksahin <sahin.kevin@gmail.com>
2020-06-17 15:29:20 +0200

    Initial Commit

---
+++ test_proxyscrape.py
@@ -0,0 +1,194 @@
+import time
+import requests
+import random
+import urllib3
+import base64
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import socket
+import sys
+from multiprocessing.dummy import Pool as ThreadPool
+from bs4 import BeautifulSoup
+
+"""
+This script was used to perform the benchmark for this blog post: 
+https://www.scrapingbee.com/blog/best-free-proxy-list-web-scraping/"
+
+In order to run it you'll need:
+Python3
+pip install requests
+pip install beautifulsoup4
+
+A ScrapingBee API key you can get one for free with 1000 credits here: https://www.scrapingbee.com/ 
+"""
+
+# IMPORTANT
+SCRAPINGBEE_API_KEY = ""
+if SCRAPINGBEE_API_KEY == "":
+    print('You need to specify a ScrapingBee API key, you can get one at: https://www.scrapingbee.com/')
+    sys.exit(0)
+#choose between freeproxy, proxyscrape and freeproxylist
+proxy_list = 'freeproxy'
+#choose between instagram, google and amazon, top1000
+website_to_test = 'top1000'
+
+
+most_common_user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+]
+
+
+def get_proxy_from_freeproxylist():
+    proxies = ''
+    counter = 1
+    has_next_page = True
+    r = requests.get(
+        url="https://app.scrapingbee.com/api/v1/",
+        params={
+            "api_key": SCRAPINGBEE_API_KEY,
+            "url": f'http://freeproxylists.net/?c=&pt=&pr=HTTPS&a%5B%5D=2&u=0', 
+            "render_js": "True", 
+        },
+        
+    )
+    counter += 1
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    lines = soup.select('table.DataGrid tbody tr')
+    for i in lines[1:]: 
+        
+        if not 'adsbygoogle' in i.text:
+            proxy = i.select('td a')[0].text
+            port = i.select('td')[1].text
+            proxies += proxy + ':' + port + '\n'
+    return proxies
+def get_proxy_from_freeproxy_cz():
+
+    proxies = ''
+    counter = 1
+    has_next_page = True
+    while(has_next_page):
+        r = requests.get(
+            url="https://app.scrapingbee.com/api/v1/",
+            params={
+                "api_key": SCRAPINGBEE_API_KEY,
+                "url": f'http://free-proxy.cz/en/proxylist/country/all/http/ping/level1/{counter}', 
+                "render_js": "false", 
+            },
+            
+        )
+        counter += 1
+        print(r.text)
+        if 'Server responded with 401' in r.text:
+            has_next_page = False
+            break
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+        lines = soup.select('table#proxy_list tbody tr')
+        #import pdb;pdb.set_trace()
+        for i in lines: 
+            if not 'adsbygoogle' in i.text:
+                base64_ip = i.select('td')[0].text.split('decode("')[1].split('"')[0]
+                base64_bytes = base64_ip.encode('ascii')
+                message_bytes = base64.b64decode(base64_bytes)
+                message = message_bytes.decode('ascii')
+                port = i.select('td')[1].select('span')[0].text
+                proxies += message + ':' + port + '\n'
+    return proxies
+
+
+if proxy_list == 'proxyscrape':
+    url = 'https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=yes&anonymity=elite'
+    r = requests.get(url, allow_redirects=True)
+    proxies_string = r.text
+elif proxy_list == 'freeproxy': 
+    proxies_string = get_proxy_from_freeproxy_cz()
+elif proxy_list == 'freeproxylist':
+    proxies_string = get_proxy_from_freeproxylist()
+else:
+    print('Proxyprovider not supported, the options are: freeproxy, proxyscrape and freeproxylist')
+
+open('http_proxies.txt', 'w').write(proxies_string)
+print(proxies_string)
+print('http_proxies.txt file written to disk')
+
+
+
+proxy_list = open("http_proxies.txt", "r").read().split("\n") * 500
+
+def call_amazon(url):
+    random_proxy = random.choice(proxy_list)
+    # random_proxy = "62.210.75.50:13732"
+    payload = {
+        "proxies": {"http": random_proxy, "https": random_proxy},
+        "url": url,
+        "verify": False,
+        "timeout": 60,
+        "headers": {
+            "User-Agent": random.choice(most_common_user_agents),
+            "referrer": "https://www.google.com",
+        },
+    }
+    
+    try:
+        start = time.time()
+        resolved_url = ''
+        response = requests.get(**payload)
+        status_code = response.status_code
+        resolved_url = response.url
+        if 200 <= status_code <= 299 or status_code == 404:
+            body = response.text
+        else:
+            body = f"Server responded with {status_code}"
+    except Exception as e:
+        body, status_code = str(e), 500
+        print("-------")
+        print(time.time() - start, random_proxy, body, e)
+        print("-------")
+        return {"statusCode": 500, "body": body, "time": time.time() - start, "resolved_url": resolved_url}
+    finally:
+        return {"statusCode": status_code, "body": body, "time": time.time() - start,  "resolved_url": resolved_url}
+
+
+total_requests = 1000
+
+blocked = 0
+concurrency = 50
+error_status_code = 0
+blocked_message = 0
+success_requests = 0
+banned_ips = {}
+banned_ips = set()
+pool = ThreadPool(concurrency)
+
+lines = open(f'{website_to_test}.csv', "r").read().split("\n") * 10
+urls = lines[:total_requests]
+
+results = pool.map(call_amazon, urls)
+pool.close()
+pool.join()
+times = []
+print(f"Results size: {len(results)}")
+
+for i in results:
+    if i["statusCode"] != 200 or "Server responded with 429" in i["body"] or "automated queries" in i["body"] or "Amazon CAPTCHA" in i["body"] or '/accounts/login/' in i["resolved_url"]:
+        if i["statusCode"] != 200:
+            error_status_code += 1
+        if "Server responded with 429" in i["body"] or "automated queries" in i["body"] or "Amazon CAPTCHA" in i["body"] or '/accounts/login/' in i["resolved_url"]:
+            blocked_message += 1
+        blocked += 1
+    else:
+        success_requests += 1
+        times.append(i["time"])
+
+
+print(f"Concurrency:  {concurrency}")
+print(f"Requests sent:  {total_requests}")
+print(f"Successful requests: {success_requests}")
+print(f"Error status_code: {error_status_code} / {total_requests}")
+print(f"Blocked message : {blocked_message} / {total_requests}")
+print(f"Average time per request: {sum(times) / len(times)}")
